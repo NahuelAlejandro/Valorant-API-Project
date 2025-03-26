76224d0ed22f991edd4d9842f32636a00f65ae84
Nahuel <nahuelmoreno283@gmail.com>
2025-03-26 11:21:44 -0300

    styles agent cards modified

--- src/components/AgentCard.tsx
+++ src/components/AgentCard.tsx
@@ -3,7 +3,9 @@ import { ReactNode } from "react"
 
 const AgentCard = ({children}: {children:ReactNode} ) => {
   return (
-    <article className="w-full h-full border border-[#ff4655] rounded-lg font-mono group/item hover:cursor-pointer flex flex-col items-center bg-slate-950 overflow-hidden">
+    <article className="w-full h-full gap-1 p-2 bg-stone-900 rounded-lg font-mono group/item hover:cursor-pointer flex flex-col overflow-hidden relative 
+    after:content-[''] after:rounded-ee-lg after:absolute after:w-2 after:h-2 after:bg-transparent after:right-[134px] after:bottom-[50px] after:z-[11] after:block after:border-stone-900 after:hover:border-stone-900 after:border-b-2 after:border-r-2
+    before:content-[''] before:rounded-ee-lg before:absolute before:w-3 before:h-2 before:bg-transparent before:right-[4px] before:bottom-[84px] before:z-[11] before:block before:border-stone-900 before:hover:border-stone-900 before:border-b-4 before:border-r-4">
       {children}
     </article>
   )
--- src/components/Agents.tsx
+++ src/components/Agents.tsx
@@ -11,7 +11,7 @@ import { useSEO } from '../hooks/useSEO';
 const Agents = () => {
     useSEO({ title: "Agents" })
     const [agents, setAgents] = useState<Agent[]>([]);
-    const [loading, setLoading]= useState<boolean>(false)
+    const [loading, setLoading] = useState<boolean>(false)
     useEffect(() => {
         getAgents().then(data => {
             setLoading(true)
@@ -23,7 +23,7 @@ const Agents = () => {
     return (
         <LayoutMain>
             <section className='w-[90%] grow flex flex-col '>
-                {loading && <div className= "w-full min-h-[35vh] font-mono text-3xl flex justify-center items-center text-white"> Loading...</div> ||
+                {loading && <div className="w-full min-h-[35vh] font-mono text-3xl flex justify-center items-center text-white"> Loading...</div> ||
                     <>
                         <h2 className="text-white text-6xl font-mono self-start ">Agents</h2>
                         <ul className='grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] gap-4 py-10'>
@@ -31,11 +31,14 @@ const Agents = () => {
                                 <li key={agent.uuid}>
                                     <Link className='w-full h-fit' to={`/Agent/${agent.uuid}`}>
                                         <AgentCard>
-                                            <img className='group-hover/item:scale-110 rounded-full' src={agent.displayIcon} alt={`image of ${agent.displayName}`} />
-                                            <article className='w-full p-2 z-10 bg-stone-200 flex justify-between group-hover/item:bg-gray-400 '>
-                                                <h2 className='font-bold text-2xl' >{agent.displayName} </h2>
+                                            <img className='object-cover rounded-s-lg rounded-se-lg md:sepia-[70%] group-hover/item:sepia-0' src={agent.displayIcon} alt={`image of ${agent.displayName}`} />
+                                            <article className='p-2 w-32 absolute rounded-ss-lg rounded-se-lg border-t-4 border-l-4 border-stone-900 right-2 z-10 bottom-12 bg-stone-300 group-hover/item:bg-stone-400 flex justify-center items-center gap-2'>
+                                                <img className='w-5 h-5' src={agent.role.displayIcon} alt="" />
                                                 <small className='font-semibold text-[#ff4655]'>{agent.role.displayName}</small>
                                             </article>
+                                            <h2 className='py-1 px-2 w-full font-bold text-2xl bg-stone-300 rounded-s-lg rounded-ee-lg group-hover/item:bg-stone-400 relative
+                                            after:content-[""] after:rounded-ee-lg after:absolute after:w-2 after:h-2 after:bg-transparent after:right-[120px] after:bottom-[36px] after:z-[11] after:border-stone-300 after:group-hover/item:border-stone-400 after:border-b-4 after:border-r-4
+                                            before:content-[""] before:rounded-se-lg before:absolute before:w-3 before:h-2 before:bg-stone-300 before:group-hover/item:bg-stone-400 before:-right-[4px] before:bottom-[72px] before:z-[11] before:border-stone-900 before:border-t-4 before:border-r-4' >{agent.displayName} </h2>
                                         </AgentCard>
                                     </Link>
                                 </li>
